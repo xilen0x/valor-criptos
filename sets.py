0483366cfddd6fada5a1aa04cfdd8d114514d8b4
xilen0x <carlos.astorgax@gmail.com>
2022-05-29 18:15:42 +0200

    set file end

--- sets.py
+++ sets.py
@@ -1,9 +1,7 @@
-from sqlalchemy import true
+import os
 
-
-#set1 = {10, 20, 30, 40, 20, 80}
 set1 =set()
-set2 = {60, 70, 80, 90, 70}
+set2 = set()
 
 
 def typesOfSets(opttion):
@@ -26,28 +24,68 @@ def typesOfSets(opttion):
     else:
         print("Bad Option")
 
-status = True
-while status:
-    optionSet1 = int(input("Ingrese un valor para almacenar en el set1:"))
-    set1.add(optionSet1)
-    yesOrNot = input("Desea ingresar otro valor?: (y/n)").lower()
-    if yesOrNot == "y" or status == "yes":
-        continue
-    elif yesOrNot == "n" or yesOrNot == "not":
-        print(set1)
-        status = False
+
+def statusSets(sett):
+    
+    status = True
+
+    while status:
+        optionSet = int(input("Ingrese un valor para almacenar en el set{}:".format(sett)))
+        
+        if sett == 1:
+            set1.add(optionSet)
+            print(set1)
+            yesOrNot = input("Desea ingresar otro valor?: (y/n)").lower()
+            if yesOrNot == "y" or yesOrNot == "yes":
+                continue
+            elif yesOrNot == "n" or yesOrNot == "not":
+                print(set1)
+                status = False
+            else:
+                print("Opción incorrecta!")
+                status = False
+        elif sett ==2:
+            set2.add(optionSet)
+            print(set2)
+            yesOrNot = input("Desea ingresar otro valor?: (y/n)").lower()
+            if yesOrNot == "y" or yesOrNot == "yes":
+                continue
+            elif yesOrNot == "n" or yesOrNot == "not":
+                print(set2)
+                status = False
+            else:
+                print("Opción incorrecta!")
+                status = False
+
+
+def setChoice():
+    os.system("clear")
+    print("""
+    1.- {}
+    2.- {} """.format(set1, set2))
+    opcion1 = int(input("Escoja un set de datos vacío: "))
+    if opcion1 == 1:
+        statusSets(opcion1) 
+        os.system("clear")
+    elif opcion1 == 2:
+        statusSets(opcion1) 
+        os.system("clear")
     else:
-        print("Opción incorrecta!")
-        status = False
+        print("No existe ese set de datos!")
+    print(set1, set2)
 
 #print(set1)
-print(set2)
+#print(set2)
+setChoice()
+setChoice()
 print("""
+SELECCIONE UNA OPCION:
+
         1.- Unión (Selecciona todos los elementos de ambos sets, dejando solo 1 elemento en el caso de repetirse alguno)
         2.- Intersección (Selección de solo los elementos comunes en ambos sets y eliminando repetidos)
         3.- Diferencia (Todos los elementos del primer set, eliminando aquellos elementos comunes con el set2 y eliminando repetidos)
         4.- Diferencia simétrica (Todos los elementos de ambos sets, eliminando aquellos elementos comunes y eliminando repetidos)
         """)
 
-opttion = int(input("Ingrese una opción:"))
+opttion = int(input("Opción: "))
 typesOfSets(opttion)
